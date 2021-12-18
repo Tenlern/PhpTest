ed3a159e6c9d9cb366e2e894eef919e6a3885dc0
Tenlern <illyash20009@yandex.ru>
2021-12-19 00:49:47 +0300

    Add README and fix first task

--- 1/script.js
+++ 1/script.js
@@ -1,6 +1,10 @@
-const currency = [ 'USD', 'EUR', 'SEK', 'JPY', 'CAD']
+const currency = [ 'USD', 'EUR', 'SEK', 'JPY', 'CAD'] // Ключи выбранных валют
 
-
+/*
+* Обновление сводки по температуре в Москве
+* Выводит данные в подготовленный заранее блок
+* Использует WeatherAPI
+*/
 function updateTemperature() {
     $.ajax({
         url:"http://api.weatherapi.com/v1/current.json",
@@ -11,6 +15,7 @@ function updateTemperature() {
             'lang': 'Ru'
         },
         success: function(response) {
+            // Используем класс Date для перевода в нужный нам формат
             date = new Date(response.location.localtime)
             $("#js-date").text(`${date.getDate()}.${date.getMonth()+1}`)
             $("#js-temperature").text(response.current.temp_c)
@@ -22,12 +27,16 @@ function updateTemperature() {
         }
     })
 }
-
+/*
+* Обновление курса обмена для пяти валют
+* Отправляет запрос к api Центробанка и заполняет блок по шаблону
+*/
 function updateCurrency() {
     $.ajax({
         url:"https://www.cbr-xml-daily.ru/daily_json.js",
         dataType: 'json',
         success: function(response) {
+            // Очищаем блок перед работой
             $('#js-currency').html('')
             currency.forEach((element) => {
                 data = response.Valute[element]
@@ -41,7 +50,8 @@ function updateCurrency() {
     })
 }
 
-$("#update").click((event) => {
+// Привязываем функции к событиям клика по кнопке и загрузки страницы
+$("#js-update").click((event) => {
     updateTemperature()
     updateCurrency()
     alert("Обновлено")
